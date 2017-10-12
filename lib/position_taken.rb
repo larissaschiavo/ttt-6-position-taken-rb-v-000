7c5c28b8edff615806da899fe1f2df18070511ec
Larissa S <larissafschiavo@gmail.com>
2017-10-12 19:52:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,12 @@
 
 def position_taken?(board, index)
 
-  
+    if board[index] == " " || board[index] == ""
+      return false 
+    elsif board[index] == nil
+      return false
+    elsif board[index] == "X" || board[index] == "O"
+      return true 
+    end  
 
 end
